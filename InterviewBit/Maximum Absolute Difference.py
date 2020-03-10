bfe4264db3987bbf29f20615f34335f26ffac706
sahiltgi <abhi28005@gmail.com>
2020-03-10 10:29:05 +0530

    Maimum Absolute Difference 1st try.

---
+++ InterviewBit/Maximum Absolute Difference.py
@@ -0,0 +1,11 @@
+def Maximum_Absolute_Difference(Value_Container):
+    if len(Value_Container) <= 1:
+        return 0
+    elif len(Value_Container) > 1:
+        for i in range():
+            pass
+
+
+if __name__ == "__main__":
+    Value_Container = list(map(int,input().split()))
+    Maximum_Absolute_Difference(Value_Container)
