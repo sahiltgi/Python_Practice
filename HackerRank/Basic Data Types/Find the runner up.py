4557df74daf31bdca0d22d128d8acd48c00d6a63
sahiltgi <abhi28005@gmail.com>
2020-03-16 19:32:29 +0530

    Hackerrank Start

---
+++ HackerRank/Basic Data Types/Find the runner up.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    largest = max(arr)
+    for i in range(n):
+        if largest == max(arr):
+            arr.remove(max(arr))
+    print(max(arr))
