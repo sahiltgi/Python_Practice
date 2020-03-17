8a79b07f475f69dd8b6a2b32a71778bc2fa7ae9a
sahiltgi <abhi28005@gmail.com>
2020-03-17 09:13:34 +0530

    Solving hacker rank basic data type problems

---
+++ HackerRank/Basic Data Types/ListComprehension.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    arr = []
+    for X in range(x + 1):
+        for Y in range(y + 1):
+            for Z in range(z + 1):
+                if X + Y + Z != n:
+                    arr.append([X, Y, Z])
+    print(arr)
