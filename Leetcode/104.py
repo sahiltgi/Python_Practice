c20c6d72aec1321cfcf51a7446d1e1f6dcda56fd
sahiltgi <abhi28005@gmail.com>
2020-03-25 22:56:49 +0530

    MAXIMUM DEPTH OF BINARY TREE

---
+++ Leetcode/104.py
@@ -0,0 +1,19 @@
+# MAXIMUM DEPTH OF BINARY TREE
+
+def maxDepth(self, root) -> int:
+    if (root == None):
+        return 0
+    if (root.left == None) and (root.right == None):
+        return 1
+    else:
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        if (l > r):
+            return l + 1
+        else:
+            return r + 1
+
+
+if __name__ == "__main__":
+    root = list(map(int, input().split()))
+    maxDepth(root)
