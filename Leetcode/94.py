2d8f69c8d6f687160b1b5e19cab23b30ffcb6e2e
sahiltgi <abhi28005@gmail.com>
2020-03-25 22:43:58 +0530

    binary tree inorder traversal

---
+++ Leetcode/94.py
@@ -0,0 +1,15 @@
+# BINARY TREE INORDER TRAVERSAL
+
+def inorderTraversal(self, root):
+    if not root:
+        return []
+    result = []
+    result.extend(self.inorderTraversal(root.left))
+    result.append(root.val)
+    result.extend(self.inorderTraversal(root.right))
+    return result
+
+
+if __name__ == "__main__":
+    root = list(map(int, input().split()))
+    inorderTraversal(root)
