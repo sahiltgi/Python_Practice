51926912bfbe70d21c46d855f6b3579541f0fed5
sahiltgi <abhi28005@gmail.com>
2020-03-25 22:37:31 +0530

    maximum subarray

---
+++ Leetcode/53.py
@@ -0,0 +1,14 @@
+# MAXIMUM SUBARRAY
+
+def maxSubArray(nums):
+    current_max = global_max = nums[0]
+    for i in range(1, len(nums)):
+        current_max = max(nums[i], nums[i] + current_max)
+        if current_max > global_max:
+            global_max = current_max
+    return global_max
+
+
+if __name__ == "__main__":
+    nums = list(map(int, input().split()))
+    (maxSubArray(nums))
